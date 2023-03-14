c568d3d713a82b8fcb3f32c6f392cc69ff124b8a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-13 23:43:23 -0300

    3.2. Data Types pt2
    https://doc.rust-lang.org/book/ch03-02-data-types.html

--- main.rs
+++ main.rs
@@ -1,80 +1,23 @@
 use std::io;
 
 fn main() {
-/*
-    let guess: u32 = "42".parse().expect("Not a number!");
+    let a = [1, 2, 3, 4, 5];
 
-    let x = 2.0; // f64
-    let y: f32 = 3.0; // f32
-*/
+    println!("Please enter an array index.");
 
-    // Addition
-    let sum = 5 + 10;
-    println!("The value of sum is: {sum}");
+    let mut index = String::new();
 
-    // Subtraction
-    let difference = 95.5 - 4.3;
-    println!("The value of difference is: {difference}");
+    // Read line
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-    // Multiplication
-    let product = 4 * 30;
-    println!("The value of product is: {product}");
+    let index: usize = index
+        .trim() // String formatting
+        .parse() // Parse to usize with explicit type annotation
+        .expect("Index entered was not a number");
 
-    // Division
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
-    println!("The value of quotient is: {quotient}");
-    println!("The value of truncated is: {truncated}");
+    let element = a[index];
 
-    // Remainder
-    let remainder = 43 % 5;
-    println!("The value of remainder is: {remainder}");
-
-    // Bool
-    let t = true;
-    let f: bool = false; // with explicit type annotation
-    println!("The value of t is: {t}");
-    println!("The value of f is: {f}");
-
-    // Char
-    let w = 'z';
-    let z: char = 'ℤ'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
-    println!("The value of w is: {w}");
-    println!("The value of z is: {z}");
-    println!("The value of heart_eyed_cat is: {heart_eyed_cat}");
-
-    // The Tuple Type
-    let tup = (500, 6.4, 1);
-    let (x, _y, _c) = tup;
-    println!("The value of x is: {x}");
-
-    {
-
-    println!("Shadowing ...");
-
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = x.0;
-    println!("The value of five_hundred is: {five_hundred}");
-
-    let six_point_four = x.1;
-    println!("The value of six_point_four is: {six_point_four}");
-
-    let one = x.2;
-    println!("The value of one is: {one}");
-
-    //println!("{x}"); ^ `(i32, f64, u8)` cannot be formatted with the default formatter
-
-    }
-
-    let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
-
-    let january = months[0];
-    
-    println!("months[0] = {january}");
-
-
-
+    println!("The value of the element at index {index} is: {element}");
 }

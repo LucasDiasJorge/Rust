2358baf1fbdb9b3e7c84c53c569ffe32bd232418
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-16 00:20:05 -0300

    3.3. Functions pt2
    https://doc.rust-lang.org/book/ch03-03-how-functions-work.html

--- main.rs
+++ main.rs
@@ -1,11 +1,32 @@
-use std::io;
-
 fn main() {
-    println!("Hello, world!");
+    
+    another_function(5);
+    print_labeled_measurement(5, 'h');
 
-    another_function();
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let y = plus_one(5);
+
+    println!("The value of y is: {y}");
+
 }
 
-fn another_function() {
-    println!("Another function.");
+//Parameters
+fn another_function(z: i32) {
+    println!("The value of z is: {z}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+// Functions with Return Values
+fn five() -> i32 {
+    return 5;
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }

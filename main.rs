20a3b0588464203527f5bc495487eb2e00990fa5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-16 23:29:17 -0300

    3.5. Control Flow
    https://doc.rust-lang.org/book/ch03-05-control-flow.html#handling-multiple-conditions-with-else-if

--- main.rs
+++ main.rs
@@ -1,80 +1,107 @@
-use std::io;
-
 fn main() {
-/*
-    let guess: u32 = "42".parse().expect("Not a number!");
+    let mut number = 3;
 
-    let x = 2.0; // f64
-    let y: f32 = 3.0; // f32
-*/
-
-    // Addition
-    let sum = 5 + 10;
-    println!("The value of sum is: {sum}");
-
-    // Subtraction
-    let difference = 95.5 - 4.3;
-    println!("The value of difference is: {difference}");
-
-    // Multiplication
-    let product = 4 * 30;
-    println!("The value of product is: {product}");
-
-    // Division
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
-    println!("The value of quotient is: {quotient}");
-    println!("The value of truncated is: {truncated}");
-
-    // Remainder
-    let remainder = 43 % 5;
-    println!("The value of remainder is: {remainder}");
-
-    // Bool
-    let t = true;
-    let f: bool = false; // with explicit type annotation
-    println!("The value of t is: {t}");
-    println!("The value of f is: {f}");
-
-    // Char
-    let w = 'z';
-    let z: char = 'ℤ'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
-    println!("The value of w is: {w}");
-    println!("The value of z is: {z}");
-    println!("The value of heart_eyed_cat is: {heart_eyed_cat}");
-
-    // The Tuple Type
-    let tup = (500, 6.4, 1);
-    let (x, _y, _c) = tup;
-    println!("The value of x is: {x}");
-
-    {
-
-    println!("Shadowing ...");
-
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = x.0;
-    println!("The value of five_hundred is: {five_hundred}");
-
-    let six_point_four = x.1;
-    println!("The value of six_point_four is: {six_point_four}");
-
-    let one = x.2;
-    println!("The value of one is: {one}");
-
-    //println!("{x}"); ^ `(i32, f64, u8)` cannot be formatted with the default formatter
-
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
     }
 
-    let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+    if number != 0{
+        println!("Number was something other than zero");
+    }
 
-    let january = months[0];
+    {
+        number = 6;
+        if number % 4 == 0 {
+            println!("number is divisible by 4");
+        } else if number % 3 == 0 {
+            println!("number is divisible by 3");
+        } else if number % 2 == 0 {
+            println!("number is divisible by 2");
+        } else {
+            println!("number is not divisible by 4, 3, or 2");
+        }
+    }
     
-    println!("months[0] = {january}");
+    // "Var w if condiction"
+    {
+        let condition = true;
+        number = if condition { 5 } else { 6 };
+        println!("The value of number is: {number}");
+    }
+
+    /*
+    loop {
+        println!("again!");
+    }
+    */
+
+    let mut counter = 0;
 
 
+    // "Var loop value"
+    let result = loop {
+        counter += 1;
 
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    // Declare names to loops and break then by name
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+
+    {
+        number = 3;
+
+        while number != 0 {
+            println!("{number}!");
+    
+            number -= 1;
+        }
+    
+        println!("LIFTOFF!!!");
+    }
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }

0d63f051c47733b7075d9ecb26d580ed40b18f2a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-11-15 19:29:34 -0300

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{get, web, App, HttpServer, Responder};
-
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, world!"
-}
+// src/main.rs
+use actix_web::{App, HttpServer};
+use actix_web::web;
+mod controllers;
+mod models;
+mod services;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
     HttpServer::new(|| {
-        App::new().service(index)
+        App::new()
+            .service(web::resource("/user/{id}").route(web::get().to(controllers::get_user_by_id)))
     })
     .bind("127.0.0.1:8080")?
     .run()

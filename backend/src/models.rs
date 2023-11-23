120d94b84cdf01e863c5fe7abe316fc28656b823
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-11-23 10:47:00 -0300

    Revert "Create models.rs"
    This reverts commit 7449ba53140a3bb4698386c8e9e81b5681fd1cf5.

--- backend/src/models.rs
+++
@@ -1,9 +0,0 @@
-// src/models.rs
-use serde_derive::Serialize;
-use serde_derive::Deserialize;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    pub id: u64,
-    pub username: String,
-}

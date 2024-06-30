a07d41b6d9f44dde985f9bbec005ada12e9e9227
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-06-30 19:06:44 -0300

    Refactoring

---
+++ guessing-game/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Lucas\OneDrive\Área de Trabalho\Github\Rust\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Lucas\OneDrive\Área de Trabalho\Github\Rust\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
--- guessing_game/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lucas\OneDrive\Área de Trabalho\Github\Rust\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\Lucas\OneDrive\Área de Trabalho\Github\Rust\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:

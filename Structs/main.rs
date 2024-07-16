bfc87ec96c290875bc6ea88ea457bcaaf0b50b1c
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-07-15 23:28:31 -0300

    Update main.rs

--- Structs/main.rs
+++ Structs/main.rs
@@ -14,10 +14,26 @@ fn user_factory(username: String, email: String) -> User{
     }
 }
 
-fn main() {
+fn user_update_name(username: String, mut user: User) -> User{
+    user.username = username;
+    return user;
+}
+
+fn user_update_email(email: String, mut user: User) -> User{
+    user.email = email;
+    return user;
+}
+
+fn main(){
     
-    let user1: User = user_factory("Lucas Dias Jorge".to_string(), "lucas_jorg@hotmail.com".to_string());
+    let mut user1: User = user_factory("Lucas Dias Jorge".to_string(), "lucas_jorg@hotmail.com".to_string());
 
     println!("\t Username: {} \n\t Email: {}",user1.username, user1.email);
-    return;
+    
+    user1 = user_update_name("Luisa Dias Jorge".to_string(), user1);
+    user1 = user_update_email("luisa_dias@hotmail.com".to_string(), user1);
+    
+    println!("\t Username: {} \n\t Email: {}",user1.username, user1.email);
+    
+    return ;
 }

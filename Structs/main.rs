df1533e0cc79742d03e7285bf034788d8e246421
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-07-15 23:21:06 -0300

    Creating User Factory

--- Structs/main.rs
+++ Structs/main.rs
@@ -5,13 +5,19 @@ struct User {
     sign_in_count: u64,
 }
 
-fn main() {
-    let user1 = User {
+fn user_factory(username: String, email: String) -> User{
+    User {
         active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
+        username: username,
+        email: email,
         sign_in_count: 1,
-    };
+    }
+}
+
+fn main() {
+    
+    let user1: User = user_factory("Lucas Dias Jorge".to_string(), "lucas_jorg@hotmail.com".to_string());
+
     println!("\t Username: {} \n\t Email: {}",user1.username, user1.email);
     return;
 }
